0327324551bac1698bf7dd744d960df0837969b6
Shadow <borntodevelop77@gmail.com>
2022-05-14 17:51:53 +0530

    add function

--- src/MyComponent/Body.js
+++ src/MyComponent/Body.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import { useState } from 'react';
-import { Input, Space } from 'antd';
-
-const { Search } = Input;
-const onSearch = value => console.log(value);
+import React from "react";
+import { useState } from "react";
 
 
 export default function Body() {
+  const [count, setCount] = useState(null);
+  const [frequency, setFrequency] = useState(null);
 
-    const [value,setCount] = useState();
+
+  const onChange = (value) => {
+    setCount(value.target.value);
+
+  };
+
+  function freq(str) {
+    console.log(str)
+    let obj = {};
+    for (let i = 0; i < str.length; i++) {
+      if (obj[str[i]] === undefined) {
+        obj[str[i]] = 1;
+      } else {
+        obj[str[i]] += 1;
+      }
+    }
+    setFrequency(JSON.stringify(obj));
+  }
 
 
-    return(
-    <Space direction="vertical">
-    <Search
-      placeholder="Enter text"
-      allowClear
-      enterButton="Calculate"
-      size="large"
-      onSearch={onSearch}
-    />
 
-  </Space>
-);
+  return (
+    <div>
+      <input type="text" placeholder="Enter name" onChange={onChange} />
+      <h3>
+        {count === null ? count : count === "" ? 0 : count.split(" ").length}
+      </h3>
+      <br />
+      <p>
+      {count === null ? count : count === "" ? 0 : freq(count)}
+      {/* {frequency===null ? "nothing":frequency} */}
+      </p>
+    </div>
+  );
 }
